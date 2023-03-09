8df671e17846b330ebe622105b624c2236ae79e9
Roman Jaquez <roman.jaquez@philips.com>
2023-03-09 15:21:16 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flungry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flungry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FluffyApp());
+}
+
+class FluffyApp extends StatelessWidget {
+  const FluffyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FluffyAppHome()
+    );
+  }
+}
+
+
+class FluffyAppHome extends StatefulWidget {
+  const FluffyAppHome({Key? key}) : super(key: key);
+
+  @override
+  State<FluffyAppHome> createState() => _FluffyAppHomeState();
+}
+
+class _FluffyAppHomeState extends State<FluffyAppHome> with TickerProviderStateMixin {
+
+  late AnimationController ctrl;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ctrl = AnimationController(vsync: this,
+      duration: const Duration(seconds: 3)
+    )..forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    double squareDim = 100;
+
+    double xPos = ((MediaQuery.of(context).size.width / squareDim) / 2); // - ((1 / (squareDim / 2)));
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset(xPos, -1.0),
+            end: Offset(xPos, (MediaQuery.of(context).size.height / squareDim))
+          ).animate(CurvedAnimation(parent: ctrl, curve: Curves.linear)),
+          child: Container(
+            width: squareDim,
+            height: squareDim,
+            color: Colors.red,
+          ),
+            )
+        ],
+      ),
+    );
+  }
+}
