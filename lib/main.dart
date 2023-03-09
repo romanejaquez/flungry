c37d275d0b577bb8952b61eff64885f2dd168f44
Roman Jaquez <roman.jaquez@philips.com>
2023-03-09 16:18:31 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class FluffyAppHome extends StatefulWidget {
   State<FluffyAppHome> createState() => _FluffyAppHomeState();
 }
 
-class _FluffyAppHomeState extends State<FluffyAppHome> with TickerProviderStateMixin {
+class _FluffyAppHomeState extends State<FluffyAppHome> with SingleTickerProviderStateMixin {
 
   late AnimationController ctrl;
 
@@ -42,7 +42,8 @@ class _FluffyAppHomeState extends State<FluffyAppHome> with TickerProviderStateM
 
     double squareDim = 100;
 
-    double xPos = ((MediaQuery.of(context).size.width / squareDim) / 2); // - ((1 / (squareDim / 2)));
+    double normalizedDim = (MediaQuery.of(context).size.width / squareDim);
+    double xPos = normalizedDim / 2; // - ((1 / normalizedDim) / 2);
 
     return Scaffold(
       body: Stack(
@@ -51,6 +52,53 @@ class _FluffyAppHomeState extends State<FluffyAppHome> with TickerProviderStateM
           position: Tween<Offset>(
             begin: Offset(xPos, -1.0),
             end: Offset(xPos, (MediaQuery.of(context).size.height / squareDim))
+          ).animate(CurvedAnimation(parent: ctrl, curve: Curves.linear)),
+          child: Container(
+            width: squareDim,
+            height: squareDim,
+            color: Colors.red,
+          ),
+            )
+        ],
+      ),
+    );
+  }
+}
+
+class FlungrySecondScreen extends StatefulWidget {
+  FlungrySecondScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FlungrySecondScreen> createState() => _FlungrySecondScreenState();
+}
+
+class _FlungrySecondScreenState extends State<FlungrySecondScreen> with SingleTickerProviderStateMixin {
+  late AnimationController ctrl;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ctrl = AnimationController(vsync: this,
+      duration: const Duration(seconds: 3)
+    )..forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    double squareDim = 100;
+
+    double normalizedDim = (MediaQuery.of(context).size.height / squareDim);
+    double xPos = normalizedDim / 2; // - ((1 / normalizedDim) / 2);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SlideTransition(
+          position: Tween<Offset>(
+            begin: Offset(-1.0, xPos),
+            end: Offset((MediaQuery.of(context).size.width / squareDim), xPos)
           ).animate(CurvedAnimation(parent: ctrl, curve: Curves.linear)),
           child: Container(
             width: squareDim,
